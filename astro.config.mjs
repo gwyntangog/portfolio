9bc694f672efb83c038fee1c43d51b45a4943ba0
Gwyneth Margaux Tangog <gwynt@mit.edu>
2026-07-20 15:33:23 -0400

    fixed base

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://gwyntangog.github.io',
-  base: '/gwyntangog/portfolio', // set to '/' if using a <username>.github.io repo
+  base: '/portfolio', // set to '/' if using a <username>.github.io repo
   output: 'static',
 });
